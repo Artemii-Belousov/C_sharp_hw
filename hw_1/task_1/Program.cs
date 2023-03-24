837fd0ce023347d9beb8a5a8f08740159f0b74d7
Artemii Belousov <artemii.belousov@mail.ru>
2023-03-25 03:11:25 +0500

    added folder hw_1

---
+++ hw_1/task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число больше, а какое меньше.
+
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+if(a > b )
+{
+    Console.Write("Число а " + "бльше чем число b");
+}
+else
+{
+    Console.Write("Число b " + " бльше чем число a");
+}
---
+++ seminar1/Program.cs
@@ -0,0 +1,18 @@
+﻿int i = 0;
+int y = 0;
+
+if (int.TryParse(Console.ReadLine(), out i) && int.TryParse(Console.ReadLine(), out y))
+{
+  if (i%y == 0)
+  {
+    Console.WriteLine("Yes");
+  }
+  else
+  {
+    Console.WriteLine("No");
+  }
+}
+else
+{
+Console.WriteLine("OOps");
+}
