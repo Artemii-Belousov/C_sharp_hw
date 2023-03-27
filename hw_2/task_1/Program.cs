f8a76c7eedebd84b2a244533f0d06c7de1dff7ce
Artemii Belousov <artemii.belousov@mail.ru>
2023-03-28 03:15:48 +0500

    task 1 is completed

--- hw_2/task_1/Program.cs
+++ hw_2/task_1/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+int number = GetNumber();
+int result = FindNum(number);
+System.Console.WriteLine(result);
+
+int GetNumber(){
+    System.Console.WriteLine("Введите число: ");
+    return int.Parse(System.Console.ReadLine());
+}
+
+int FindNum(int num){
+    int FirstNum = num % 100;
+    int MidleNum = FirstNum / 10;
+    return MidleNum;
+}
+
+
