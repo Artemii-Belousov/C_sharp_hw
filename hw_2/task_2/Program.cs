896db9ecc3813138b26902769fe9d1a2ed42de14
Artemii Belousov <artemii.belousov@mail.ru>
2023-03-29 15:24:06 +0500

    changed task_2 folder

--- hw_2/task_2/Program.cs
+++ hw_2/task_2/Program.cs
@@ -5,38 +5,39 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-void Main(){
+void Main()
+{
     Print();
 }
 
-void Print(){
-   
+void Print()
+{
+
     int sum = GetResult();
     System.Console.WriteLine("Result: " + sum);
 }
 
-int GetNumder(){
+int GetNumder()
+{
     System.Console.WriteLine("Enter a nunber: ");
     return int.Parse(System.Console.ReadLine());
 }
 
 
-int GetResult(){
+int GetResult()
+{
     int num = GetNumder();
-    if(num < 99){
-        System.Console.WriteLine("You entered a number less then 100!");
-        System.Console.WriteLine("There is no third digit!");
-    }
-    else if(num >= 100 && num < 1000){
-        System.Console.WriteLine("You entered a number more then 100!");
-        num = num % 100;
+    if (num > 99 && num < 999)
+    {
         num = num % 10;
     }
     else if(num >= 1000 && num < 10000){
-        // System.Console.WriteLine("You entered a number more then 134500!");
-        num = num % 1000;
-        num = num % 100;
         num = num / 10;
+        num = num % 10;
+    }
+    else if(num >= 10000 && num < 100000){
+        num = num / 100;
+        num = num % 10;
     }
 
     return num;
