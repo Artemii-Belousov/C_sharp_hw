0726b321d4c4c12f09ddcfae69c3fdee58205459
Artemii Belousov <artemii.belousov@mail.ru>
2023-03-29 12:26:47 +0500

    added several chenges

--- hw_1/task_4/Program.cs
+++ hw_1/task_4/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные чсла от 1 до N.
 
 
-Console.Write("Введите число: ");
-int n = int.Parse(Console.ReadLine());
 
+System.Console.WriteLine("Enter a number: ");
+int n = int.Parse(System.Console.ReadLine());
 //  При использовании цикла While необходимо объявить переменную преред циклом, а при использовании цикла for эта переменная объявляется внутри условия.
 
 //  При помощи цикла while
@@ -23,7 +23,8 @@ for (int i = 1; i <= n; i++)
 {    
     if(i % 2 == 0)
     {
-        Console.Write(i);
-        Console.Write(" ");
+        System.Console.WriteLine(i);
+        System.Console.WriteLine(" ");
+        
     }
 }
---
+++ hw_2/task_2/Program.cs
@@ -0,0 +1,46 @@
+﻿
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+void Main(){
+    Print();
+}
+
+void Print(){
+   
+    int sum = GetResult();
+    System.Console.WriteLine("Result: " + sum);
+}
+
+int GetNumder(){
+    System.Console.WriteLine("Enter a nunber: ");
+    return int.Parse(System.Console.ReadLine());
+}
+
+
+int GetResult(){
+    int num = GetNumder();
+    if(num < 99){
+        System.Console.WriteLine("You entered a number less then 100!");
+        System.Console.WriteLine("There is no third digit!");
+    }
+    else if(num >= 100 && num < 1000){
+        System.Console.WriteLine("You entered a number more then 100!");
+        num = num % 100;
+        num = num % 10;
+    }
+    else if(num >= 1000 && num < 10000){
+        // System.Console.WriteLine("You entered a number more then 134500!");
+        num = num % 1000;
+        num = num % 100;
+        num = num / 10;
+    }
+
+    return num;
+}
+
+Main();
+
