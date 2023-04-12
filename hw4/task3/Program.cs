e5b5ccd0eefc3d7340e4ba7930594016444cb031
Artemii Belousov <artemii.belousov@mail.ru>
2023-04-13 00:24:26 +0500

    solving task 3 of workshop 4

---
+++ hw4/task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int GetNum()
+{
+    System.Console.WriteLine("Введите длинну массива: ");
+    return int.Parse(System.Console.ReadLine());
+}
+
+int num = GetNum();
+
+int[] array = new int[num];
+
+Method(num);
+
+void Method(int num)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+    System.Console.WriteLine(string.Join(", ", array));
+
+}
+
+
+
