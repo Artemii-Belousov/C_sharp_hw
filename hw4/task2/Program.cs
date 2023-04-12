97b9730d4648c3d8174f711264c2430bdc868be3
Artemii Belousov <artemii.belousov@mail.ru>
2023-04-12 23:32:25 +0500

    solving task 2 of workshop 4

---
+++ hw4/task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetNumber()
+{
+    System.Console.WriteLine("Введите число: ");
+    return int.Parse(System.Console.ReadLine());
+}
+
+int num =  GetNumber();
+
+int SumNumber(int num){
+    
+    int counter = Convert.ToString(num).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = num - num % 10;
+      result = result + (num - advance);
+      num = num / 10;
+    }
+   return result;
+  }
+
+  int sumNumber = SumNumber(num);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
