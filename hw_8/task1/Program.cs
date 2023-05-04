674c7d5e2e54ba7de009a0562fce068cb3614e48
Artemii Belousov <artemii.belousov@mail.ru>
2023-05-04 20:32:40 +0500

    solving task 2 of workshop 8

---
+++ hw_8/task1/Program.cs
@@ -0,0 +1,136 @@
+﻿// // Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// // Например, задан массив:
+// // 1 4 7 2
+// // 5 9 2 3
+// // 8 4 2 4
+// // В итоге получается вот такой массив:
+// // 7 4 2 1
+// // 9 5 3 2
+// // 8 4 4 2
+
+// int n = GetNumber("кличество строк");
+// int m = GetNumber("количество столбцов");
+
+// int[,] Array = new int[n, m];
+
+// int GetNumber(string arg)
+// {
+//     System.Console.WriteLine($"Введите {arg}: ");
+//     return int.Parse(Console.ReadLine());
+// }
+// int [,] CreateArray(int[,] Array )
+// {
+    
+//     for (int i = 0; i < Array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < Array.GetLength(1); j++)
+//         {
+//             Array[i, j] = new Random().Next(0, 10);
+//         }
+//     }
+//     return Array;
+// }
+
+// void PrintArray(int[,] Array )
+// {
+//     for (int i = 0; i < Array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < Array.GetLength(1); j++)
+//         {
+//             System.Console.Write($"{Array[i, j]} ");            
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+// void DecreasingNumber(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       for (int k = 0; k < array.GetLength(1) - 1; k++)
+//       {
+//         if (array[i, k] < array[i, k + 1])
+//         {
+//           int temp = array[i, k + 1];
+//           array[i, k + 1] = array[i, k];
+//           array[i, k] = temp;
+//         }
+//       }
+//     }
+//   }
+// }
+
+// CreateArray(Array);
+// PrintArray(Array);
+
+// System.Console.WriteLine();
+
+// // CreateArray(Array);
+// // PrintArray(Array);
+// DecreasingNumber(Array);
+
+
+Console.WriteLine($"\nВведите размер массива m x n и диапазон случайных значений:");
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int range = InputNumbers("Введите диапазон: от 1 до ");
+
+int[,] array = new int[m, n];
+CreateArray(array);
+WriteArray(array);
+
+System.Console.WriteLine();
+Console.WriteLine($"Отсортированный массив: ");
+OrderArrayLines(array);
+WriteArray(array);
+
+void OrderArrayLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = int.Parse(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(1, range);
+    }
+  }
+}
+
+void WriteArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
