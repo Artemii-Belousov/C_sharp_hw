f7574036c2a905891087b2dcc32bcece40252153
Artemii Belousov <artemii.belousov@mail.ru>
2023-05-09 22:53:17 +0500

    solving task 1 of workshop 9

---
+++ hw_9/task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+int n = InputInt("Введите положительное число");
+int m = 1;
+
+
+if (n < 1)
+{
+    System.Console.WriteLine("Ввели не положительное число.");
+}
+System.Console.WriteLine(NaturalNamber(n, m));
+
+int NaturalNamber(int n, int m)
+{
+    if (n == m) return n;
+
+    else System.Console.Write($"{NaturalNamber(n, m + 1)}, ");
+
+    return m;
+}
+
+
+int InputInt(string input)
+{
+    System.Console.WriteLine(input);
+    return int.Parse(Console.ReadLine());
+}
+
+
