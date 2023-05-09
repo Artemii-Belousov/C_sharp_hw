2637bdee83535163ec89dc8e03ec1c28c7f0e889
Artemii Belousov <artemii.belousov@mail.ru>
2023-05-09 23:05:41 +0500

    solving task 2 of workshop 9

---
+++ hw_9/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int m = InputInt("Введите число M");
+int n = InputInt("Введите число N");
+System.Console.WriteLine($"Сумма элементов от {m} до {n} = {CountNaturalNum(m, n)}");
+
+int CountNaturalNum(int m, int n)
+{
+    if(m == n) return n;
+    return n + CountNaturalNum(m, n - 1);
+}
+
+int InputInt(string input)
+{
+    System.Console.WriteLine(input);
+    return int.Parse(Console.ReadLine());
+}
+
